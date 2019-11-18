7e577301e332715fab7a0ea6421a393689941baf
mcguffin <joern@flyingletters.com>
2019-11-18 13:55:56 +0100

    Release 0.0.23

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ Plugin Name: Gitish Updated
 Plugin URI: http://wordpress.org/
 Description: Enter description here.
 Author: Jörn Lund
-Version: 0.0.22
+Version: 0.0.23
 Author URI: https://github.com/mcguffin
 License: GPL3
 Github Repository: mcguffin/gitish-updated
